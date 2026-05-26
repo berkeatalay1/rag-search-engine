db704f682aaccd739a0dc400199fe4cccf737b5f
berke.atalay1 <berke.atalay@beakwise.com>
2026-05-26 13:27:05 +0300

    Refactor keyword search CLI to utilize search_command and implement movie loading from JSON

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
+from lib.keyword_search import search_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    search_parser = subparsers.add_parser("search", help="Search movies using keywords")
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
 
     match args.command:
         case "search":
-            print(f"Searching for: {args.query}")
-            movies = json.load(open("data/movies.json"))['movies']
-            results = [movie for movie in movies if args.query.lower() in movie["title"].lower()]
-            for i, result in enumerate(results, start=1):
-                print(f"{i}. {result['title']}")
-            pass
+            print("Searching for:", args.query)
+            results = search_command(args.query)
+            for i, res in enumerate(results, 1):
+                print(f"{i}. {res['title']}")
         case _:
             parser.print_help()
+
 
 if __name__ == "__main__":
     main()
